d05d4c59031fbed0d69eb58b779d5de56c6ec0ba
Unknown <m.hennen@student.fontys.nl>
2018-08-08 21:48:43 +0200

    register optimalize

--- client/app/scripts/index.js
+++ client/app/scripts/index.js
@@ -30,6 +30,66 @@ app.config(function ($routeProvider){
     })
     .controller("loginController", function ($scope) {
     })
-    .controller("registerController", function ($scope) {
+    .controller("registerController", function ($scope, $log) {
+        $scope.registerUser = function () {
+
+            $scope.errorMatchPassword = "";
+            var user = {
+                name : "",
+                email : "",
+                password : ""
+            };
+
+            this.checkPasswords();
+            this.validPassword($scope.user.password);
+
+            if($scope.validPasswords){
+                user = $scope.user;
+                $log.info(user);
+            }
+            else {
+                $scope.errorMatchPassword = "Passwords don't match!"
+            }
+
+
+        };
+
+
+
+        $scope.checkPasswords = function () {
+            var password = $scope.user.password;
+            var confirm = $scope.user.confirm;
+
+            $scope.validPasswords = password === confirm;
+
+            $log.info($scope.validPasswords)
+        };
+
+        $scope.validPassword = function (password) {
+
+            $log.info(password);
+            $scope.errorValidPassword = "";
+
+            var error = "Password must contain 8 characters \n" +
+                "Password must contain an uppercase \n" +
+                "Password must contain an lowercase \n" +
+                "Password must contain an number \n";
+
+            if(password.length < 8){
+                $scope.errorValidPassword = error;
+            }
+
+            else if(password.match(/[A-z]/)){
+                $scope.errorValidPassword = error;
+            }
+
+            else if(password.match(/[a-z]/)){
+                $scope.errorValidPassword = error;
+            }
+
+            else if(password.match(/[A-Z]/)){
+                $scope.errorValidPassword = error;
+            }
+        }
 
     });
